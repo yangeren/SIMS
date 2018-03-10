e94343f1ee150d9d177624da313939cc8188cebf
hanz <xiaoyaoyangeren@163.com>
2018-03-10 12:05:03 +0800

    init

---
+++ src/main/java/com/supermarket/module/Product.java
@@ -0,0 +1,15 @@
+package com.supermarket.module;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+    private static final long serialVersionUID = 748392348L;
+
+    private String pnumber;
+    private String category;
+    private String name;
+    private float price;
+
+
+}
